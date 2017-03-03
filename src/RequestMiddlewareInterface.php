8022a3ec3d3e11a20e1d4f82e98b03ad9190f1a3
Pavel Hudinsky <p.hudinsky@gmail.com>
2017-03-03 11:12:21 +0300

    =Add ability to set middlewares

--- src/RequestMiddlewareInterface.php
+++ src/RequestMiddlewareInterface.php
@@ -1,15 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: p.hudinsky
- * Date: 3/3/2017
- * Time: 10:19 AM
- */
 
 namespace Bambora;
 
+use Bambora\Exception\BamboraRequestException;
+use Bambora\Request\BaseRequest;
+use Bambora\Response\BaseResponse;
 
 interface RequestMiddlewareInterface
 {
+    /**
+     * @param BaseRequest $request
+     * @param array $normalizedRequest
+     */
+    public function onRequest(BaseRequest $request, array $normalizedRequest);
 
+    /**
+     * @param BaseRequest $request
+     * @param BamboraRequestException $exception
+     */
+    public function onException(BaseRequest $request, BamboraRequestException $exception);
+
+    /**
+     * @param BaseResponse $response
+     * @param array $normalizedResponse
+     */
+    public function onResponse(BaseResponse $response, array $normalizedResponse);
 }
