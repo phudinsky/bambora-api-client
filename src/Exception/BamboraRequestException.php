9d596b35d37028883efdc743caffaad9f8d2f870
Pavel Hudinsky <p.hudinsky@gmail.com>
2017-03-03 11:11:49 +0300

    =Create request exception

--- src/Exception/BamboraRequestException.php
+++ src/Exception/BamboraRequestException.php
@@ -4,4 +4,41 @@ namespace Bambora\Exception;
 
 class BamboraRequestException extends BamboraException
 {
+
+    /**
+     * @var array
+     */
+    private $requestData;
+    /**
+     * @var array|null
+     */
+    private $responseData;
+
+    public function __construct(
+        $message,
+        array $requestData,
+        array $responseData = null,
+        $code = 0,
+        \Exception $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->requestData = $requestData;
+        $this->responseData = $responseData;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequestData(): array
+    {
+        return $this->requestData;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResponseData(): array
+    {
+        return $this->responseData;
+    }
 }
